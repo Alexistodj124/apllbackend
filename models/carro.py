63733effd900d17ad7f93627b9ab61458f9aafeb
Alexistodj124 <alexistodj@gmail.com>
2024-11-14 13:06:51 -0600

    include logo erro fix

--- models/carro.py
+++ models/carro.py
@@ -1,4 +1,5 @@
 from utils.db import db
+import base64
 
 class Carro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,11 +15,12 @@ class Carro(db.Model):
         self.linea = linea
         self.marca_id = marca_id
 
-    def to_dict(self):
-        return {
+    def to_dict(self, include_logo=True):
+        carro_data = {
             'id': self.id,
             'modelo': self.modelo,
             'linea': self.linea,
             'marca_id': self.marca_id,
-            'marca': self.marca.to_dict()
+            'marca': self.marca.to_dict(include_logo=include_logo) if self.marca else None
         }
+        return carro_data
